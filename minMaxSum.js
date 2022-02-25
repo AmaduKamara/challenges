112763c01ba184562305f8dc9f87a3d8c11a0a56
Amkam <amaduskamara@gmail.com>
2022-02-25 07:40:50 +0000

    Create minMaxSum function

---
+++ minMaxSum.js
@@ -0,0 +1,14 @@
+const miniMaxSum = (arr) => {
+    // Write your code here
+    let min = 0;
+    let max = 0;
+    arr.sort();
+    for(let i = 0; i < arr.length -1; i++){
+        min += arr[i];
+    }
+    for(let i = 1; i < arr.length; i++){
+        max += arr[i]
+    }
+    
+    console.log(min, max);
+}
